bf5a678117bc6bf07bcf4acae73975b10ddc29de
dennis.grewe <dennis.grewe@gmail.com>
2013-10-21 11:56:18 +0200

    File to test the javascript eval method in node.js

---
+++ eval_method_test/eval_test.js
@@ -0,0 +1,5 @@
+/**
+ * This is a test file to test node.js for eval() method support. 
+ * If this is the case, then node.js will write "Hello Node.js" to the console.*/
+
+eval(console.log("Hello Node.js!"));
